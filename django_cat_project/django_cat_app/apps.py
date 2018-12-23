1010c53c03ac450cc733e9fbaa35af9e04e9d318
Nikolas Engelhard <nikolas.engelhard@gmail.com>
2018-12-23 23:24:45 +0100

    django project

---
+++ django_cat_project/django_cat_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DjangoCatAppConfig(AppConfig):
+    name = 'django_cat_app'
---
+++ django_cat_project/django_cat_app/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+
+class UserLog(models.Model):
+    user_id = models.CharField(max_length=100)
+    cat_count = models.IntegerField(default=0)
